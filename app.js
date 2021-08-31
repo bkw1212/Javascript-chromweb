18067fc01e0a0615528c99bfdc6d6cfdb8beaac4
bkw1212 <bkw1212@naver.com>
2021-08-31 22:05:21 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const title = document.querySelector("div.hello:first-child h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
 
-function handleMouseEnter(){
-    title.innerText = "Mouse is here!";
-    title.style.color = "blue";
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add("hidden");
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-function handleMouseLeave(){
-    title.innerText = "Mouse is gone!";
-    title.style.color = "green";
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleWindowResize(){
-    title.innerText = "You just resized!";
-    title.style.color = "red";
+const savedUsername =localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit);
+} else {
+    paintGreetings(username);
 }
-
-function handleRightClick(){
-    title.innerText = "That was right click!";
-    title.style.color = "indigo";
-}
-
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("contextmenu", handleRightClick);
